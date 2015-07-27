4cffde75b182b493067f0f8a5d162a68980d103a
Konstantin Shaposhnikov <k.shaposhnikov@gmail.com>
2015-07-28 00:17:44 +0800

    parquetformat: implement Read for PageHeader

--- parquetformat/io.go
+++ parquetformat/io.go
@@ -6,9 +6,17 @@ import (
 	"git.apache.org/thrift.git/lib/go/thrift"
 )
 
+func newProtocol(r io.Reader) *thrift.TCompactProtocol {
+	ttransport := thrift.NewStreamTransportR(r)
+	return thrift.NewTCompactProtocol(ttransport)
+}
+
 // FileMetaData.Read reads the object from a io.Reader
 func (meta *FileMetaData) Read(r io.Reader) error {
-	var ttransport = thrift.NewStreamTransportR(r)
-	var tprotocol = thrift.NewTCompactProtocol(ttransport)
-	return meta.read(tprotocol)
+	return meta.read(newProtocol(r))
+}
+
+// PageHeader.Read reads the object from a io.Reader
+func (ph *PageHeader) Read(r io.Reader) error {
+	return ph.read(newProtocol(r))
 }
