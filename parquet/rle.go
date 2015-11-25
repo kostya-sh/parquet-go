22f77876f851679d2dd7ea8ae69012846b07b7bb
Konstantin Shaposhnikov <k.shaposhnikov@gmail.com>
2015-11-25 22:37:59 +0800

    parquet: more RLE decoding tests
    Also a few cleanups/fixes to RLE decoder

--- parquet/rle.go
+++ parquet/rle.go
@@ -24,16 +24,18 @@ import (
 // repeated-value := value that is repeated, using a fixed-width of round-up-to-next-byte(bit-width)
 
 type rleDecoder struct {
-	data  []byte
 	width int
-	e     error
-	i     int
-	eod   bool
+
+	data []byte
+	pos  int
+	e    error
+	eod  bool
 
 	// rle
 	rleCount uint32
 	rleValue int32
-	//header  uint32 // uint32 is enough because parquet-mr uses java int
+
+	// bit-packed
 }
 
 func newRLEDecoder(data []byte, width int) *rleDecoder {
@@ -48,40 +50,41 @@ func newRLEDecoder(data []byte, width int) *rleDecoder {
 
 func (d *rleDecoder) readRLERunValue() {
 	byteWidth := (d.width + 7) / 8 // TODO: remember this in d
-	n := d.i + byteWidth
+	n := d.pos + byteWidth
 	if n > len(d.data) {
-		d.e = fmt.Errorf("cannot read RLE run value (no more data)")
+		d.e = fmt.Errorf("cannot read RLE run value (not enough data)")
 		return
 	}
 	switch byteWidth {
 	case 1:
-		d.rleValue = int32(d.data[d.i])
+		d.rleValue = int32(d.data[d.pos])
 	case 2:
-		d.rleValue = int32(binary.LittleEndian.Uint16(d.data[d.i:n]))
+		d.rleValue = int32(binary.LittleEndian.Uint16(d.data[d.pos:n]))
 	case 3:
-		b3 := d.data[d.i]
-		b2 := d.data[d.i+1]
-		b1 := d.data[d.i+2]
+		b3 := d.data[d.pos]
+		b2 := d.data[d.pos+1]
+		b1 := d.data[d.pos+2]
 		d.rleValue = int32(b3) + int32(b2)<<8 + int32(b1)<<16
 	case 4:
-		d.rleValue = int32(binary.LittleEndian.Uint32(d.data[d.i:n]))
+		d.rleValue = int32(binary.LittleEndian.Uint32(d.data[d.pos:n]))
 	default:
 		panic("should not happen")
 	}
-	d.i = n
+	d.pos = n
 }
 
 func (d *rleDecoder) readRunHeader() {
-	if d.i >= len(d.data) {
+	if d.pos >= len(d.data) {
 		d.eod = true
 		return
 	}
 
-	h, n := binary.Uvarint(d.data[d.i:])
+	h, n := binary.Uvarint(d.data[d.pos:])
 	if n <= 0 || h > math.MaxUint32 {
-		d.e = fmt.Errorf("failed to read RLE run header at pos %d. Uvarint result: (%d, %d)", d.i, h, n)
+		d.e = fmt.Errorf("failed to read RLE run header at pos %d. Uvarint result: (%d, %d)", d.pos, h, n)
+		return
 	}
-	d.i += n
+	d.pos += n
 	if h&1 == 1 {
 		// bit packed run
 		panic("nyi")
@@ -92,14 +95,17 @@ func (d *rleDecoder) readRunHeader() {
 }
 
 func (d *rleDecoder) nextInt32() int32 {
+	var next int32
 	if d.rleCount > 0 {
+		next = d.rleValue
 		d.rleCount--
 		if d.rleCount == 0 {
 			d.readRunHeader()
 		}
-		return d.rleValue
+	} else {
+		panic("nyi")
 	}
-	panic("nyi")
+	return next
 }
 
 func (d *rleDecoder) hasNext() bool {
--- parquet/rle_test.go
+++ parquet/rle_test.go
@@ -1,39 +1,48 @@
 package parquet
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
-// Single RLE run: 1-bit per value, 10 x 0
-func TestSinlgeRLERun_10x0_1bit(t *testing.T) {
-	d := newRLEDecoder([]byte{0x14, 0x00}, 1)
-
-	for i := 0; i < 10; i++ {
-		if !d.hasNext() {
-			t.Fatalf("want 10 values, got %d", i)
-		}
-		i32 := d.nextInt32()
-		if i32 != 0 {
-			t.Errorf("value #%d: got %d, want 0", i+1, i32)
-		}
+func rleDecodeAll(w int, data []byte) (a []int32, err error) {
+	d := newRLEDecoder(data, w)
+	for d.hasNext() {
+		a = append(a, d.nextInt32())
 	}
-	if d.hasNext() {
-		t.Fatalf("Got more than 10 values")
-	}
+	return a, d.err()
 }
 
-// Single RLE run: 20-bits per value, 300x1
-func TestSinlgeRLERun_300x1_20bit(t *testing.T) {
-	d := newRLEDecoder([]byte{0xD8, 0x04, 0x01, 0x00, 0x00}, 20)
-
-	for i := 0; i < 300; i++ {
-		if !d.hasNext() {
-			t.Fatalf("want 300 values, got %d", i)
-		}
-		i32 := d.nextInt32()
-		if i32 != 1 {
-			t.Errorf("value #%d: got %d, want 1", i+1, i32)
-		}
+func repeatInt32(count int, value int32) (a []int32) {
+	for i := 0; i < count; i++ {
+		a = append(a, value)
 	}
-	if d.hasNext() {
-		t.Fatalf("Got more than 300 values")
+	return
+}
+
+var rle32Tests = []struct {
+	width  int
+	data   []byte
+	values []int32
+}{
+	// Single RLE run: 1-bit per value, 10 x 0
+	{1, []byte{0x14, 0x00}, repeatInt32(10, 0)},
+
+	// Single RLE run: 20-bits per value, 300x1
+	{20, []byte{0xD8, 0x04, 0x01, 0x00, 0x00}, repeatInt32(300, 1)},
+
+	// 2 RLE runs: 1-bit per value, 10x0, 9x1
+	{1, []byte{0x14, 0x00, 0x12, 0x01}, append(repeatInt32(10, 0), repeatInt32(9, 1)...)},
+}
+
+func TestRLEDecoder(t *testing.T) {
+	for i, test := range rle32Tests {
+		values, err := rleDecodeAll(test.width, test.data)
+		if err != nil {
+			t.Errorf("test %d. unexpected error: %s", i, err)
+		}
+		if !reflect.DeepEqual(values, test.values) {
+			t.Errorf("test %d. got %v, want %v", i, values, test.values)
+		}
 	}
 }
