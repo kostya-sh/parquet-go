163b3f0b1f95f17fb97988a0c0c0a4790b6c00a5
Konstantin Shaposhnikov <k.shaposhnikov@gmail.com>
2015-12-23 20:20:04 +0800

    parquet: add tests for boolean and bytearray column chunk readers
    Booleans.paruet generated by gen.BooleansGen from parquet-test as of date of
    this commit.
    
    ByteArrays.parquet generated by gen.ByteArraysGen.

--- parquet/column_test.go
+++ parquet/column_test.go
@@ -1,79 +1,141 @@
 package parquet
 
 import (
-	"fmt"
 	"os"
+	"reflect"
 	"testing"
+
+	"github.com/kostya-sh/parquet-go/parquetformat"
 )
 
-func TestBooleanColumnReader(t *testing.T) {
-	r, err := os.Open("../../parquet-test/harness/input/Booleans.parquet")
-	//f, err := os.Open("/home/ksh/downloads/nation.impala.parquet")
+type cell struct {
+	d int
+	r int
+	v interface{}
+}
+
+func checkColumnValues(t *testing.T, path string, c int, expected []cell) {
+	r, err := os.Open(path)
 	if err != nil {
-		fmt.Printf("Error: %s\n", err)
+		t.Errorf("failed to open %s: %s", path, err)
 		return
 	}
-	// err = getColumnReader(f, 0)
-	// fmt.Printf("Error: %s\n", err)
+	defer r.Close()
 
 	m, err := ReadFileMetaData(r)
 	if err != nil {
-		t.Fatalf("%s", err)
+		t.Errorf("failed to read metadata: %s", err)
+		return
 	}
 	schema, err := SchemaFromFileMetaData(m)
 	if err != nil {
-		t.Fatalf("%s", err)
+		t.Errorf("failed to create schema: %s", err)
+		return
 	}
 
-	c := 2
-	for _, rg := range m.RowGroups {
+	k := 0
+	for i, rg := range m.RowGroups {
 		cc := rg.Columns[c]
 		cs := schema.ColumnByPath(cc.MetaData.PathInSchema)
-		cr, err := NewBooleanColumnChunkReader(r, cs, cc)
+		var cr ColumnChunkReader
+		switch cs.SchemaElement.GetType() {
+		case parquetformat.Type_BOOLEAN:
+			cr, err = NewBooleanColumnChunkReader(r, cs, cc)
+		case parquetformat.Type_BYTE_ARRAY:
+			cr, err = NewByteArrayColumnChunkReader(r, cs, cc)
+		default:
+			panic("nyi")
+		}
 		if err != nil {
-			t.Fatalf("%s", err)
+			t.Errorf("column %d: failed to create reader for row group %d: %s", c, i, err)
+			return
 		}
 		for cr.Next() {
-			fmt.Printf("V:%v\tD:%d\tR:%d\n", cr.Value(), cr.Levels().D, cr.Levels().R)
+			if k < len(expected) {
+				got := cell{cr.Levels().D, cr.Levels().R, cr.Value()}
+				if !reflect.DeepEqual(got, expected[k]) {
+					t.Errorf("column %d: value at pos %d = %#v, want %#v", c, k, got, expected[k])
+				}
+			}
+			k++
+			//fmt.Printf("V:%v\tD:%d\tR:%d\n", cr.Value(), cr.Levels().D, cr.Levels().R)
 		}
 		if cr.Err() != nil {
-			t.Fatalf("%s", cr.Err())
+			t.Errorf("column %d: failed to read row group %d: %s", c, i, cr.Err())
 		}
+	}
+	if k != len(expected) {
+		t.Errorf("column %d: read %d values, want %d values", c, k, len(expected))
 	}
 }
 
-func TestByteArrayColumnReader(t *testing.T) {
-	r, err := os.Open("../../parquet-test/harness/input/ByteArrays.parquet")
-	//f, err := os.Open("/home/ksh/downloads/nation.impala.parquet")
-	if err != nil {
-		fmt.Printf("Error: %s\n", err)
-		return
-	}
-	// err = getColumnReader(f, 0)
-	// fmt.Printf("Error: %s\n", err)
+func TestBooleanColumnChunkReader(t *testing.T) {
+	checkColumnValues(t, "testdata/Booleans.parquet", 0, []cell{
+		{0, 0, true},
+		{0, 0, true},
+		{0, 0, false},
+		{0, 0, true},
+		{0, 0, false},
+		{0, 0, true},
+	})
 
-	m, err := ReadFileMetaData(r)
-	if err != nil {
-		t.Fatalf("%s", err)
-	}
-	schema, err := SchemaFromFileMetaData(m)
-	if err != nil {
-		t.Fatalf("%s", err)
-	}
+	checkColumnValues(t, "testdata/Booleans.parquet", 1, []cell{
+		{0, 0, false},
+		{1, 0, false},
+		{1, 0, true},
+		{1, 0, true},
+		{0, 0, false},
+		{1, 0, true},
+	})
 
-	c := 2
-	for _, rg := range m.RowGroups {
-		cc := rg.Columns[c]
-		cs := schema.ColumnByPath(cc.MetaData.PathInSchema)
-		cr, err := NewByteArrayColumnChunkReader(r, cs, cc)
-		if err != nil {
-			t.Fatalf("%s", err)
-		}
-		for cr.Next() {
-			fmt.Printf("V:%v\tD:%d\tR:%d\n", string(cr.ByteArray()), cr.Levels().D, cr.Levels().R)
-		}
-		if cr.Err() != nil {
-			t.Fatalf("%s", cr.Err())
-		}
-	}
+	checkColumnValues(t, "testdata/Booleans.parquet", 2, []cell{
+		{0, 0, false},
+
+		{0, 0, false},
+
+		{1, 0, true},
+
+		{1, 0, true},
+		{1, 1, false},
+		{1, 1, true},
+
+		{0, 0, false},
+		{1, 0, true},
+	})
+}
+
+func TestByteArrayColumnChunkReader(t *testing.T) {
+	checkColumnValues(t, "testdata/ByteArrays.parquet", 0, []cell{
+		{0, 0, []byte{'r', '1'}},
+		{0, 0, []byte{'r', '2'}},
+		{0, 0, []byte{'r', '3'}},
+		{0, 0, []byte{'r', '4'}},
+		{0, 0, []byte{'r', '5'}},
+		{0, 0, []byte{'r', '6'}},
+	})
+
+	checkColumnValues(t, "testdata/ByteArrays.parquet", 1, []cell{
+		{0, 0, []byte(nil)},
+		{1, 0, []byte{'o', '2'}},
+		{1, 0, []byte{'o', '3'}},
+		{1, 0, []byte{'o', '4'}},
+		{0, 0, []byte(nil)},
+		{1, 0, []byte{'o', '6'}},
+	})
+
+	checkColumnValues(t, "testdata/ByteArrays.parquet", 2, []cell{
+		{0, 0, []byte(nil)},
+
+		{0, 0, []byte(nil)},
+
+		{1, 0, []byte{'p', '3', '_', '1'}},
+
+		{1, 0, []byte{'p', '4', '_', '1'}},
+		{1, 1, []byte{'p', '4', '_', '2'}},
+		{1, 1, []byte{'p', '4', '_', '3'}},
+
+		{0, 0, []byte(nil)},
+
+		{1, 0, []byte{'p', '6', '_', '1'}},
+	})
 }
