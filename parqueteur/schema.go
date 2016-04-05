32ac91c9361a878ce73ca1284e3bb4d7fbec7111
Fabrizio (Misto) Milo <mistobaan@gmail.com>
2016-04-04 18:20:21 -0700

    add schema command to command line utility

--- parqueteur/schema.go
+++ parqueteur/schema.go
@@ -1,34 +1,31 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/kostya-sh/parquet-go/parquet"
+)
+
 var cmdSchema = &Command{
 	Name: "schema",
 	Help: "display parquet file schema",
 }
 
 func init() {
-	//cmdSchema.Run = runSchema
+	cmdSchema.Run = runSchema
 }
 
-// func runSchema(cmd *Command, args []string) error {
-// 	if len(args) != 1 {
-// 		return fmt.Errorf("No files")
-// 	}
+func runSchema(cmd *Command, args []string) error {
+	if len(args) != 1 {
+		return fmt.Errorf("No files")
+	}
 
-// 	r, err := os.Open(args[0])
-// 	if err != nil {
-// 		return err
-// 	}
-// 	defer r.Close()
+	r, err := parquet.OpenFile(args[0])
+	if err != nil {
+		return err
+	}
+	defer r.Close()
 
-// 	meta, err := readFileMetaData(r)
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	schema, err := schemaFromFileMetaData(meta)
-// 	if err != nil {
-// 		return err
-// 	}
-// 	_, err = fmt.Println(schema.DisplayString())
-// 	return err
-// }
+	_, err = fmt.Println(r.Schema().DisplayString())
+	return err
+}
