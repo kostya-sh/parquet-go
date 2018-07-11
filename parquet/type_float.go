a5ce60157dc9a35228850cefa444da2b8ab953b5
Konstantin Shaposhnikov <k.shaposhnikov@gmail.com>
2018-07-11 08:21:40 +0100

    parquet: clean up FLOAT decoders code
    - de-duplicate code
    - avoid overflows
    - make code a bit easier to follow

--- parquet/type_float.go
+++ parquet/type_float.go
@@ -2,9 +2,29 @@ package parquet
 
 import (
 	"encoding/binary"
-	"fmt"
+	"errors"
 	"math"
 )
+
+type floatDecoder interface {
+	decodeFloat32(dst []float32) error
+}
+
+func decodeFloat(d floatDecoder, dst interface{}) error {
+	switch dst := dst.(type) {
+	case []float32:
+		return d.decodeFloat32(dst)
+	case []interface{}:
+		b := make([]float32, len(dst), len(dst))
+		err := d.decodeFloat32(b)
+		for i := 0; i < len(dst); i++ {
+			dst[i] = b[i]
+		}
+		return err
+	default:
+		panic("invalid argument")
+	}
+}
 
 type floatPlainDecoder struct {
 	data []byte
@@ -18,40 +38,22 @@ func (d *floatPlainDecoder) init(data []byte) error {
 	return nil
 }
 
-func (d *floatPlainDecoder) decode(slice interface{}) error {
-	switch buf := slice.(type) {
-	case []float32:
-		return d.decodeFloat32(buf)
-	case []interface{}:
-		return d.decodeE(buf)
-	default:
-		panic("invalid argument")
-	}
+func (d *floatPlainDecoder) decode(dst interface{}) error {
+	return decodeFloat(d, dst)
 }
 
-func (d *floatPlainDecoder) decodeFloat32(buf []float32) error {
-	i := 0
-	for i < len(buf) && d.pos < len(d.data) {
-		if d.pos+4 > len(d.data) {
-			return fmt.Errorf("float/plain: not enough data")
+func (d *floatPlainDecoder) decodeFloat32(dst []float32) error {
+	for i := 0; i < len(dst); i++ {
+		if d.pos >= len(d.data) {
+			return errNED
 		}
-		buf[i] = math.Float32frombits(binary.LittleEndian.Uint32(d.data[d.pos:]))
+		if uint(d.pos+4) > uint(len(d.data)) {
+			return errors.New("float/plain: not enough bytes to decode a float number")
+		}
+		dst[i] = math.Float32frombits(binary.LittleEndian.Uint32(d.data[d.pos:]))
 		d.pos += 4
-		i++
-	}
-	if i == 0 {
-		return fmt.Errorf("float/plain: no more data")
 	}
 	return nil
-}
-
-func (d *floatPlainDecoder) decodeE(buf []interface{}) error {
-	b := make([]float32, len(buf), len(buf))
-	err := d.decodeFloat32(b)
-	for i := 0; i < len(buf); i++ {
-		buf[i] = b[i]
-	}
-	return err
 }
 
 type floatDictDecoder struct {
@@ -66,33 +68,17 @@ func (d *floatDictDecoder) initValues(dictData []byte, count int) error {
 	return d.dictDecoder.initValues(d.values, dictData)
 }
 
-func (d *floatDictDecoder) decode(slice interface{}) error {
-	switch buf := slice.(type) {
-	case []float32:
-		return d.decodeFloat32(buf)
-	case []interface{}:
-		return d.decodeE(buf)
-	default:
-		panic("invalid argument")
-	}
+func (d *floatDictDecoder) decode(dst interface{}) error {
+	return decodeFloat(d, dst)
 }
 
-func (d *floatDictDecoder) decodeFloat32(buf []float32) error {
-	keys, err := d.decodeKeys(len(buf))
+func (d *floatDictDecoder) decodeFloat32(dst []float32) error {
+	keys, err := d.decodeKeys(len(dst))
 	if err != nil {
 		return err
 	}
 	for i, k := range keys {
-		buf[i] = d.values[k]
+		dst[i] = d.values[k]
 	}
 	return nil
-}
-
-func (d *floatDictDecoder) decodeE(buf []interface{}) error {
-	b := make([]float32, len(buf), len(buf))
-	err := d.decodeFloat32(b)
-	for i := 0; i < len(buf); i++ {
-		buf[i] = b[i]
-	}
-	return err
 }
