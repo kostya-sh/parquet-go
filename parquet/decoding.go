aff8cd4a24a1160f8dfd4d35b35d5bc50947090e
Konstantin Shaposhnikov <k.shaposhnikov@gmail.com>
2018-07-13 08:43:00 +0100

    parquet: clarify that decoding to empty slice is not supported
    This is because implementing it will require remembering end of data
    condition in every decoder. Otherwise the correct error cannot be
    returned when decoding to am empty slice from a decoder that already
    returned errNED.

--- parquet/decoding.go
+++ parquet/decoding.go
@@ -8,20 +8,26 @@ var errNED = errors.New("not enough data to decode all values")
 type levelsDecoder interface {
 	init(data []byte)
 
-	// decodeLevels decodes exactly len(dst) level values into dst slice.
+	// decodeLevels decodes exactly len(dst) level values into dst
+	// slice. len(dst) should be > 0.
 	//
 	// It should return errNED if there is not enough data to decode all levels
 	// or another error if data is corrupted.
+	//
+	// TODO: implementations should panic when len(dst) = 0
 	decodeLevels(dst []uint16) error
 }
 
 type valuesDecoder interface {
 	init(data []byte) error
 
-	// decode decodes exactly len(dst) values into dst slice.
+	// decode decodes exactly len(dst) values into dst slice. len(dst) should be
+	// > 0.
 	//
 	// It should return errNED if there is not enough data to decode all values
 	// or another error if data is corrupted.
+	//
+	// TODO: implementations should panic when len(dst) = 0
 	decode(dst interface{}) error
 }
 
