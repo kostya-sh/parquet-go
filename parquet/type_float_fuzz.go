fd2859c35d138e67c42521213261dba18b555094
Konstantin Shaposhnikov <k.shaposhnikov@gmail.com>
2018-07-13 08:31:16 +0100

    parquet: add fuzz function for FLOAT PLAIN_DICTIONARY decoder

--- parquet/type_float_fuzz.go
+++ parquet/type_float_fuzz.go
@@ -5,12 +5,14 @@ package parquet
 import "fmt"
 import "math"
 
-func FuzzFloatPlain(data []byte) int {
+func fuzzFloat(d valuesDecoder, data []byte, initCanFail bool) int {
 	const maxSize = 10000
 
-	d := floatPlainDecoder{}
 	err := d.init(data)
 	if err != nil {
+		if initCanFail {
+			return 0
+		}
 		panic("unexpected error in init")
 	}
 	dst1 := make([]float32, maxSize, maxSize)
@@ -21,7 +23,7 @@ func FuzzFloatPlain(data []byte) int {
 
 	err = d.init(data)
 	if err != nil {
-		panic("unexpected error in init")
+		panic("unexpected error in the 2nd init")
 	}
 	dst2 := make([]interface{}, maxSize, maxSize)
 	err = d.decode(dst2)
@@ -44,4 +46,37 @@ func FuzzFloatPlain(data []byte) int {
 	}
 
 	return 1
+}
+
+func FuzzFloatPlain(data []byte) int {
+	return fuzzFloat(&floatPlainDecoder{}, data, false)
+}
+
+func FuzzFloatDict(data []byte) int {
+	dictData := []byte{
+		0x01, 0x00, 0x00, 0x00, // min floar
+		0x00, 0x00, 0xC0, 0x7F, // NaN
+		0x00, 0x00, 0x00, 0x00, // 0.0
+		0xFA, 0x3E, 0xC8, 0x42, // 100.123
+		0xFF, 0xFF, 0x7F, 0x7F, // max float
+		0x33, 0x33, 0xC8, 0x42, // 100.1
+		0x66, 0x66, 0xC8, 0x42, // 100.2
+		0x9A, 0x99, 0xC8, 0x42, // 100.3
+	}
+
+	d := &floatDictDecoder{
+		dictDecoder: dictDecoder{vd: &floatPlainDecoder{}},
+	}
+
+	r := 0
+	for i := 0; i < 16; i++ {
+		if err := d.initValues(dictData, i); err != nil {
+			break
+		}
+		println(i)
+		if fuzzFloat(d, data, true) == 1 {
+			r = 1
+		}
+	}
+	return r
 }
