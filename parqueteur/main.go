e67b7c267eba5d1ee19cbc1693ddb2c00b0caf0f
Konstantin Shaposhnikov <k.shaposhnikov@gmail.com>
2018-07-20 08:38:08 +0100

    parqueteur: add support for collecting CPU and memory profiles

--- parqueteur/main.go
+++ parqueteur/main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"os"
+	"runtime"
+	"runtime/pprof"
 )
 
 type Command struct {
@@ -30,9 +33,10 @@ var commands = []*Command{
 }
 
 func usage() {
-	fmt.Fprintf(os.Stderr, "Usage: parqueteur command [options] [parquetfile]\n\n")
+	fmt.Fprintf(os.Stderr, "Usage: parqueteur [options] command [command options] [parquetfile]\n\n")
+	flag.PrintDefaults()
 
-	fmt.Fprintf(os.Stderr, "Supported commands:\n")
+	fmt.Fprintf(os.Stderr, "\n\nSupported commands:\n")
 	for _, cmd := range commands {
 		fmt.Fprintf(os.Stderr, "\n")
 		fmt.Fprintf(os.Stderr, " %s - %s\n", cmd.Name, cmd.Help)
@@ -44,11 +48,27 @@ func usage() {
 }
 
 func main() {
-	args := os.Args[1:]
+	var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to `file`")
+	var memprofile = flag.String("memprofile", "", "write memory profile to `file`")
+
+	flag.Parse()
+	args := flag.Args()
 	if len(args) < 1 {
 		usage()
 	}
 
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal("could not create CPU profile: ", err)
+		}
+		if err = pprof.StartCPUProfile(f); err != nil {
+			log.Fatal("could not start CPU profile: ", err)
+		}
+		defer pprof.StopCPUProfile()
+	}
+
+	found := false
 	for _, cmd := range commands {
 		if cmd.Name == args[0] {
 			err := cmd.Flag.Parse(args[1:])
@@ -56,15 +76,28 @@ func main() {
 				usage()
 			}
 
+			found = true
 			err = cmd.Run(cmd, cmd.Flag.Args())
 			if err != nil {
 				fmt.Fprintf(os.Stderr, "Error: %s\n", err)
 				os.Exit(1)
 			}
-			os.Exit(0)
+			break
 		}
 	}
+	if !found {
+		usage()
+	}
 
-	// command not found
-	usage()
+	if *memprofile != "" {
+		f, err := os.Create(*memprofile)
+		if err != nil {
+			log.Fatal("could not create memory profile: ", err)
+		}
+		runtime.GC() // get up-to-date statistics
+		if err = pprof.WriteHeapProfile(f); err != nil {
+			log.Fatal("could not write memory profile: ", err)
+		}
+		_ = f.Close()
+	}
 }
